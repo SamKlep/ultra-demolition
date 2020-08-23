d2be8db6a709220a446c6d479ad935e298a8f9b6
Samuel Klepper <55067844+SamKlep@users.noreply.github.com>
2020-08-23 14:46:36 -0500

    Changed pic on contact page.

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -14,7 +14,7 @@ const Main = () => {
           <Carousel.Item>
             <img
               className='d-block w-100 carousel-image'
-              src='/assets/images/demo3-4.jpg'
+              src='/assets/images/demo3-4-resize.jpg'
               alt='First slide'
             />
           </Carousel.Item>
