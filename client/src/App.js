a5802519bd4ce107464182b4d3282851bf6f8ddc
Samuel Klepper <55067844+SamKlep@users.noreply.github.com>
2020-08-23 00:50:59 -0500

    added static.json.

--- client/src/App.js
+++ client/src/App.js
@@ -17,22 +17,16 @@ function App() {
     <Router>
       <NavBar />
       <Switch>
-        <Route exact path='/'>
-          <Home />
-        </Route>
-        <Route path='/about'>
-          <About />
-        </Route>
-        <Route path='/contact'>
-          <Contact />
-        </Route>
+        <Route exact path='/' component={Home} />
 
-        <Route path='/projects'>
-          <Projects />
-        </Route>
-        <Route path='/services'>
-          <Services />
-        </Route>
+        <Route path='/about' component={About} />
+
+        <Route path='/contact' component={Contact} />
+
+        <Route path='/projects' component={Projects} />
+
+        <Route path='/services' component={Services} />
+
         <Route component={Error404} />
       </Switch>
       <Footer />
