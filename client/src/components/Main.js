cdd9a14bf7f8a9425b1d53d9fb7188c0ae640cfb
Samuel Klepper <55067844+SamKlep@users.noreply.github.com>
2020-08-23 14:51:45 -0500

    Changed carousel sizing.

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -5,77 +5,83 @@ import { Animated } from "react-animated-css";
 const Main = () => {
   return (
     <div className='container'>
-      <Animated
-        animationIn='bounceInLeft'
-        animationOut='fadeOut'
-        isVisible={true}
-      >
-        <Carousel>
-          <Carousel.Item>
-            <img
-              className='d-block w-100 carousel-image'
-              src='/assets/images/demo3-4-resize.jpg'
-              alt='First slide'
-            />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className='d-block w-100 carousel-image'
-              src='/assets/images/demo2.jpg'
-              alt='Third slide'
-            />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className='d-block w-100 carousel-image'
-              src='/assets/images/demo4.jpg'
-              alt='Third slide'
-            />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className='d-block w-100 carousel-image'
-              src='/assets/images/demo9.jpg'
-              alt='Third slide'
-            />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className='d-block w-100 carousel-image'
-              src='/assets/images/demo16.JPG'
-              alt='Third slide'
-            />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className='d-block w-100 carousel-image'
-              src='/assets/images/dirt1.jpg'
-              alt='Third slide'
-            />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className='d-block w-100 carousel-image'
-              src='/assets/images/house1_carousel.jpg'
-              alt='Third slide'
-            />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className='d-block w-100 carousel-image'
-              src='/assets/images/machine1.jpg'
-              alt='Third slide'
-            />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className='d-block w-100 carousel-image'
-              src='/assets/images/dirt3.JPG'
-              alt='Third slide'
-            />
-          </Carousel.Item>
-        </Carousel>
-      </Animated>
+      <div className='container'>
+        <div className='container'>
+          <div className='container'>
+            <Animated
+              animationIn='bounceInLeft'
+              animationOut='fadeOut'
+              isVisible={true}
+            >
+              <Carousel>
+                <Carousel.Item>
+                  <img
+                    className='d-block w-100 carousel-image'
+                    src='/assets/images/demo3-4-resize.jpg'
+                    alt='First slide'
+                  />
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img
+                    className='d-block w-100 carousel-image'
+                    src='/assets/images/demo2-resize.jpg'
+                    alt='Third slide'
+                  />
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img
+                    className='d-block w-100 carousel-image'
+                    src='/assets/images/demo4-resize.jpg'
+                    alt='Third slide'
+                  />
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img
+                    className='d-block w-100 carousel-image'
+                    src='/assets/images/demo9.jpg'
+                    alt='Third slide'
+                  />
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img
+                    className='d-block w-100 carousel-image'
+                    src='/assets/images/demo16.JPG'
+                    alt='Third slide'
+                  />
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img
+                    className='d-block w-100 carousel-image'
+                    src='/assets/images/dirt1.jpg'
+                    alt='Third slide'
+                  />
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img
+                    className='d-block w-100 carousel-image'
+                    src='/assets/images/house1_carousel.jpg'
+                    alt='Third slide'
+                  />
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img
+                    className='d-block w-100 carousel-image'
+                    src='/assets/images/machine1.jpg'
+                    alt='Third slide'
+                  />
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img
+                    className='d-block w-100 carousel-image'
+                    src='/assets/images/dirt3.JPG'
+                    alt='Third slide'
+                  />
+                </Carousel.Item>
+              </Carousel>
+            </Animated>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
